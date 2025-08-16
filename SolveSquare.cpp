db6c192b7bb83ecd7c63343ea9a6e7f8038fb120
Lemming_S_Maslinoi <130701315+KhomenkoMichail@users.noreply.github.com>
2025-08-16 23:03:24 +0300

    Инициализация, допустимость а==0, изменен внешний цикл

--- SolveSquare.cpp
+++ SolveSquare.cpp
@@ -7,20 +7,16 @@ void skip (void);
 void solve_square (double a, double b, double c);
 int main (void)
 {
-    double a, b, c;     //коэффициенты квадратного уравнения
-    char ch;
-    intro();
-    while ((ch=getchar())!='\n')
+    double a = 0.0,
+    b = 0.0,
+    c = 0.0;     //коэффициенты квадратного уравнения
+    char ch = '0';
+    int KeepSolving = 1;
+    while (KeepSolving)
     {
-            skip();
+            intro();
             printf("Введите численное значение коэффициента a:");
             a=get_double();
-            while (a==0.0)
-            {
-                printf("При a=0 уравнение не является квадратным!\n");
-                printf("Введите другое численное значение a:");
-                a=get_double();
-            }
             printf ("Введите численное значение коэффициента b:");
             b=get_double();
             printf ("Введите численное значение коэффициента c:");
@@ -28,7 +24,11 @@ int main (void)
             printf ("Полученное уравнение:\n");
             printf ("%.2lfx^2+%.2lfx+%.2lf = 0\n", a, b, c);
             solve_square(a, b, c);
-            intro();
+            printf ("Введите любой символ, чтобы продолжить решать уравнения ");
+            printf("или нажмите [Enter], чтобы завершить программу.\n");
+            if ((ch=getchar())=='\n')
+                KeepSolving = 0;
+            else skip();
     }
     printf ("Программа завершена!\n");
     return 0;
@@ -37,18 +37,17 @@ void intro (void)
 {
     printf ("Эта программа предназначена для решения квадратных уравнений вида:\n");
     printf ("ax^2+bx+c=0\n");
-    printf ("Введите любой символ для продолжения (или пустую строку для завершения программмы:\n");
 }
 void skip (void)  //функция пропуска входных данных
 {
-    char ch;
+    char ch = '0';
     while ((ch=getchar())!='\n')
         continue;
 }
 double get_double (void)   //функция получения значения типа double
 {
-    char ch;
-    double d;
+    char ch = '0';
+    double d = 0.0;
     while ((scanf ("%lf", &d))!=1)
     {
     while ((ch=getchar())!='\n')
@@ -60,18 +59,27 @@ double get_double (void)   //функция получения значения типа double
 }
 void solve_square (double a, double b, double c)  //функция решения заданного квадратного уравнения
 {
-    double d;
-    double sq;
-    d = b*b - 4*a*c;
-    if (d<0)
-        printf ("Данное уравнение не имеет решений!\n");
-    else if (d==0.0)
-        printf ("Уравнение имеет единственный корень x = %.2lf\n", -b/(2*a));
-    else
+    double d = 0.0; //дискриминант квадратного уравнения
+    double sq = 0.0;
+    if ((a == 0.0)&&(b == 0.0)&&(c ==0.0))
+        printf ("x - любое число\n");
+    else if ((a == 0.0)&&(b==0.0))         //при с!=0
+        printf ("Уравнение не имеет корней.\n");
+    else if (a == 0.0)         //при b!=0 и с!=0
+        printf ("Уравнение имеет единственный корень x = %.2lf\n", -c/b);
+    else   //при a!=0
         {
-            printf ("Уравнение имеет два различных корня:\n");
-            sq = sqrt(d);
-            printf("x1=%.2lf\nx2=%.2lf\n", (-b-sq)/(2*a), (-b+sq)/(2*a));
+        d = b*b - 4*a*c;      //вычисление дискриминанта
+        if (d<0)
+            printf ("Уравнение не имеет корней.\n");
+        else if (d==0.0)
+            printf ("Уравнение имеет единственный корень x = %.2lf\n", -b/(2*a));
+        else   //при d>0
+            {
+                printf ("Уравнение имеет два различных корня:\n");
+                sq = sqrt(d);
+                printf("x1=%.2lf\nx2=%.2lf\n", (-b-sq)/(2*a), (-b+sq)/(2*a));
+            }
         }
 }
 
